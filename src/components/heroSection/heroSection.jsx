ae5b04a95dc2f84e6b7d2043bea21fe9cc5d6e0a
hami-code-web <hamimohhamadkhani@gamil.com>
2025-08-28 13:53:13 +0330

    hero section commit

--- src/components/heroSection/heroSection.jsx
+++ src/components/heroSection/heroSection.jsx
@@ -11,7 +11,7 @@ import banner2 from '../../assets/sliderImages/bannerImages/banner2.webp';
 
 import { IoIosArrowDown } from 'react-icons/io';
 
-const heroSection = () => {
+const HeroSection = () => {
   const images = [
     slider1,
     slider2,
@@ -26,6 +26,7 @@ const heroSection = () => {
   const nextSlide = () => {
     setCurrentSlide((prev) => (prev + 1) % images.length);
   };
+
   const prevSlide = () => {
     setCurrentSlide((prev) => (prev - 1 + images.length) % images.length);
   };
@@ -34,67 +35,67 @@ const heroSection = () => {
     const interval = setInterval(() => {
       nextSlide();
     }, 5000);
+    return () => clearInterval(interval); // Cleanup interval
   }, []);
+
   return (
     <div className="h-[550px] max-w-7xl mx-auto flex items-center gap-6">
-    <div className="h-[600px] flex justify-center items-center gap-6">
-      <div className="relative">
-        <img
-          src={images[currentSlide]}
-          className="shadow-xl cursor-pointer h-[500px] object-cover rounded-xl"
-          alt=""
-        />
+      <div className="h-[600px] flex justify-center items-center gap-6">
+        <div className="relative">
+          <img
+            src={images[currentSlide]}
+            className="shadow-xl cursor-pointer h-[500px] object-cover rounded-xl"
+            alt={`Slide ${currentSlide + 1}`}
+          />
 
-        <button
-          onClick={nextSlide}
-          className="absolute rotate-90 left-2 top-1/2 text-black rounded-full"
-        >
-          <IoIosArrowDown size={20} />
-        </button>
+          <button
+            onClick={nextSlide}
+            className="absolute rotate-90 left-2 top-1/2 text-black rounded-full"
+          >
+            <IoIosArrowDown size={20} />
+          </button>
 
-        <button
-          onClick={prevSlide}
-          className="absolute rotate-[-90deg] text-black right-2 top-1/2 rounded-full"
-        >
-          <IoIosArrowDown size={20} />
-        </button>
+          <button
+            onClick={prevSlide}
+            className="absolute rotate-[-90deg] text-black right-2 top-1/2 rounded-full"
+          >
+            <IoIosArrowDown size={20} />
+          </button>
 
-        <div className="absolute bottom-3 left-1/2 -translate-x-1/2">
-          <div className="flex gap-2">
-            {images.map((_, index) => (
-              <div
-<<<<<<< HEAD
-                key={index} 
-=======
-                key={index}
->>>>>>> 34abc3a00fa12276e2be7f50f78302dc58208691
-                onClick={() => setCurrentSlide(index)}
-                className={`w-3 h-3 rounded-full cursor-pointer ${
-                  currentSlide === index ? 'bg-red-500' : 'bg-gray-400'
-                }`}
-              ></div>
-            ))}
+          <div className="absolute bottom-3 left-1/2 -translate-x-1/2">
+            <div className="flex gap-2">
+              {images.map((_, index) => (
+                <div
+                  key={index}
+                  onClick={() => setCurrentSlide(index)}
+                  className={`w-3 h-3 rounded-full cursor-pointer ${
+                    currentSlide === index ? 'bg-red-500' : 'bg-gray-400'
+                  }`}
+                ></div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-6">
-        <div>
-          <img
-            src={banner1}
-            className="shadow-xl cursor-pointer h-[240px] object-cover rounded-xl"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            src={banner2}
-            className="shadow-xl cursor-pointer h-[240px] object-cover rounded-xl"
-            alt=""
-          />
+
+        <div className="flex flex-col gap-6">
+          <div>
+            <img
+              src={banner1}
+              className="shadow-xl cursor-pointer h-[240px] object-cover rounded-xl"
+              alt="Banner 1"
+            />
+          </div>
+          <div>
+            <img
+              src={banner2}
+              className="shadow-xl cursor-pointer h-[240px] object-cover rounded-xl"
+              alt="Banner 2"
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default heroSection;
+export default HeroSection;
